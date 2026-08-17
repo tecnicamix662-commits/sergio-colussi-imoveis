f01ed4754e94e35fa46039c71dcc26f2a7a1ea3a
Sérgio Colussi Imóveis <contato@sergiocolussi.com.br>
2026-08-17 15:33:29 -0300

    chore: adicionar rota de financiamento no sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -38,6 +38,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     {
+      url: `${baseUrl}/simular-financiamento`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/contato`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
